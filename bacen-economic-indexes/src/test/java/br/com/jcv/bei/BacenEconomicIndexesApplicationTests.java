f367e3c2ce5328830ce73f128ec6e57e361cb111
julio.vitorino <julio.vitorino@accountfy.com>
2024-05-23 09:50:29 -0300

    Opensource BEI spring boot infrastructure has been created. close #79

---
+++ bacen-economic-indexes/src/test/java/br/com/jcv/bei/BacenEconomicIndexesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jcv.bei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BacenEconomicIndexesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
