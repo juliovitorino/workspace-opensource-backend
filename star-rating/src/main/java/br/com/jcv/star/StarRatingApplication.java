1fc88d34aebc25e1c57c1779b6063b8f029a14a5
julio.vitorino <julio.vitorino@accountfy.com>
2024-05-27 12:07:05 -0300

    Star rating spring boot structure has been created. close #98

---
+++ star-rating/src/main/java/br/com/jcv/star/StarRatingApplication.java
@@ -0,0 +1,13 @@
+package br.com.jcv.star;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarRatingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarRatingApplication.class, args);
+	}
+
+}
