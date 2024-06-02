f86ec90f2acc0a993ce2993e9be62012a3f86763
julio.vitorino <julio.vitorino@accountfy.com>
2024-06-02 09:35:18 -0300

    RestClienteConsumer structure

--- rest-client-consumer/src/main/java/br/com/jcv/restclient/guardian/GuardianRestClientConsumer.java
+++ rest-client-consumer/src/main/java/br/com/jcv/restclient/guardian/GuardianRestClientConsumer.java
@@ -2,6 +2,9 @@ package br.com.jcv.restclient.guardian;
 
 import org.springframework.web.bind.annotation.RequestBody;
 
+import br.com.jcv.restclient.infrastructure.response.ControllerGenericResponse;
+
 public interface GuardianRestClientConsumer {
     String login(@RequestBody LoginRequest request) ;
+    ControllerGenericResponse askHeimdallPermission(String jwtToken,String role);
 }
