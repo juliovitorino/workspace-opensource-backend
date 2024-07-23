43df8d44143b47ab22d698002b38ad9d6933c097
julio.vitorino <julio.vitorino@accountfy.com>
2024-07-23 14:08:22 -0300

    Adding a service receiving a Soma interface implementation

--- mylab/src/main/java/br/com/br/study/mylab/lambda/SomaLambdaController.java
+++ mylab/src/main/java/br/com/br/study/mylab/lambda/SomaLambdaController.java
@@ -1,5 +1,6 @@
 package br.com.br.study.mylab.lambda;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,9 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class SomaLambdaController {
 
+    @Autowired private SomaService somaService;
+
     @GetMapping("lambda")
     public ResponseEntity<Integer> executaSoma(@Param("a") int a, @Param("b") int b, @Param("c") int c) {
-        Soma resultado = (a1, b1, c1) -> a1 + b1 + c1;
-        return ResponseEntity.ok(resultado.execute(a, b, c));
+        Soma somaImpl = (a1, b1, c1) -> a1 + b1 + c1;
+
+        //envia a implementação anonima (lambda) da interface e os parametros pra serem usados no serviço
+        return ResponseEntity.ok(somaService.adicionaMaisDez(somaImpl,a,b,c));
     }
 }
