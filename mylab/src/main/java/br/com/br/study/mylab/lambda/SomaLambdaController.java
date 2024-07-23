fa07e82937d2eee53ac99ad959a035322e937391
julio.vitorino <julio.vitorino@accountfy.com>
2024-07-23 14:34:02 -0300

    Implementing a factory pattern using lambda

--- mylab/src/main/java/br/com/br/study/mylab/lambda/SomaLambdaController.java
+++ mylab/src/main/java/br/com/br/study/mylab/lambda/SomaLambdaController.java
@@ -1,5 +1,8 @@
 package br.com.br.study.mylab.lambda;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
 import org.springframework.http.ResponseEntity;
@@ -14,10 +17,15 @@ public class SomaLambdaController {
     @Autowired private SomaService somaService;
 
     @GetMapping("lambda")
-    public ResponseEntity<Integer> executaSoma(@Param("a") int a, @Param("b") int b, @Param("c") int c) {
-        Soma somaImpl = (a1, b1, c1) -> a1 + b1 + c1;
+    public ResponseEntity<Integer> executaSoma(@Param("a") int a, @Param("b") int b, @Param("c") int c, @Param("v") int v) {
+        Map<Integer, Soma> mapSomaFactory = new HashMap<>();
+        mapSomaFactory.put(1, (a1, b1, c1) -> a1 + b1 + c1);
+        mapSomaFactory.put(2, (a1, b1, c1) -> (a1 + b1 + c1) * 2);
+        mapSomaFactory.put(3, (a1, b1, c1) -> (a1 + b1 + c1) * 3);
+        mapSomaFactory.put(4, (a1, b1, c1) -> (a1 + b1 + c1) * 4);
+        mapSomaFactory.put(5, (a1, b1, c1) -> (a1 + b1 + c1) * 5);
 
         //envia a implementação anonima (lambda) da interface e os parametros pra serem usados no serviço
-        return ResponseEntity.ok(somaService.adicionaMaisDez(somaImpl,a,b,c));
+        return ResponseEntity.ok(somaService.adicionaMaisDez(mapSomaFactory.get(v),a,b,c));
     }
 }
