46d7746298c1a98f04585c10686301280f1bc184
julio.vitorino <julio.vitorino@accountfy.com>
2025-05-06 11:28:19 -0300

    [0561128] Add regexp for period

--- treinadorpro/src/main/java/br/com/jcv/treinadorpro/corelayer/model/PersonalFeature.java
+++ treinadorpro/src/main/java/br/com/jcv/treinadorpro/corelayer/model/PersonalFeature.java
@@ -7,6 +7,7 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
+import javax.validation.constraints.Pattern;
 import java.time.LocalDateTime;
 
 @Data
@@ -25,24 +26,38 @@ public class PersonalFeature {
     private User personalUser;
 
     @Column(name = "mon_period", length = 11)
+    @Pattern(regexp = "^([01]\\d|2[0-3]):[0-5]\\d-([01]\\d|2[0-3]):[0-5]\\d$",
+            message = "The format should be HH:mm-HH:mm (i.e.: 08:00-12:00)")
     private String monPeriod;
 
     @Column(name = "tue_period", length = 11)
+    @Pattern(regexp = "^([01]\\d|2[0-3]):[0-5]\\d-([01]\\d|2[0-3]):[0-5]\\d$",
+            message = "The format should be HH:mm-HH:mm (i.e.: 08:00-12:00)")
     private String tuePeriod;
 
     @Column(name = "wed_period", length = 11)
+    @Pattern(regexp = "^([01]\\d|2[0-3]):[0-5]\\d-([01]\\d|2[0-3]):[0-5]\\d$",
+            message = "The format should be HH:mm-HH:mm (i.e.: 08:00-12:00)")
     private String wedPeriod;
 
     @Column(name = "thu_period", length = 11)
+    @Pattern(regexp = "^([01]\\d|2[0-3]):[0-5]\\d-([01]\\d|2[0-3]):[0-5]\\d$",
+            message = "The format should be HH:mm-HH:mm (i.e.: 08:00-12:00)")
     private String thuPeriod;
 
     @Column(name = "fri_period", length = 11)
+    @Pattern(regexp = "^([01]\\d|2[0-3]):[0-5]\\d-([01]\\d|2[0-3]):[0-5]\\d$",
+            message = "The format should be HH:mm-HH:mm (i.e.: 08:00-12:00)")
     private String friPeriod;
 
     @Column(name = "sat_period", length = 11)
+    @Pattern(regexp = "^([01]\\d|2[0-3]):[0-5]\\d-([01]\\d|2[0-3]):[0-5]\\d$",
+            message = "The format should be HH:mm-HH:mm (i.e.: 08:00-12:00)")
     private String satPeriod;
 
     @Column(name = "sun_period", length = 11)
+    @Pattern(regexp = "^([01]\\d|2[0-3]):[0-5]\\d-([01]\\d|2[0-3]):[0-5]\\d$",
+            message = "The format should be HH:mm-HH:mm (i.e.: 08:00-12:00)")
     private String sunPeriod;
 
     @Column(name = "status", length = 1)
