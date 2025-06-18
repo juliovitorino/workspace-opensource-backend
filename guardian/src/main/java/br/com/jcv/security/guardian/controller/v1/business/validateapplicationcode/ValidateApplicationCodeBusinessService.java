cb76b59562abcb34d9c21283312238f1ce202f17
julio.vitorino <julio.vitorino@accountfy.com>
2025-06-18 14:05:06 -0300

    [181316] deprecated comment

--- guardian/src/main/java/br/com/jcv/security/guardian/controller/v1/business/validateapplicationcode/ValidateApplicationCodeBusinessService.java
+++ guardian/src/main/java/br/com/jcv/security/guardian/controller/v1/business/validateapplicationcode/ValidateApplicationCodeBusinessService.java
@@ -4,6 +4,19 @@ import java.util.UUID;
 
 import br.com.jcv.commons.library.commodities.service.BusinessService;
 
+/**
+ * @deprecated Esta interface {@code ValidateApplicationCodeBusinessService} está obsoleta
+ * e não deve mais ser utilizada.
+ *
+ * <p>
+ * Motivo da depreciação: A validação de códigos de aplicação foi movida para um novo
+ * fluxo centralizado em {@code ValidateAccountService} .
+ * </p>
+ *
+ * <p>
+ * Recomenda-se utilizar a nova abordagem para garantir compatibilidade com futuras versões.
+ * </p>
+ */
 @Deprecated
 public interface ValidateApplicationCodeBusinessService extends BusinessService<UUID, Boolean> {
 }
