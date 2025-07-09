d2d804a86987ca9f27574a8c89b47b6e0eac4fd9
julio.vitorino <julio.vitorino@accountfy.com>
2025-07-09 11:29:49 -0300

    [091129] fix getLoggedUser

--- treinadorpro/src/main/java/br/com/jcv/treinadorpro/corebusiness/users/GetLoggedUserServiceImpl.java
+++ treinadorpro/src/main/java/br/com/jcv/treinadorpro/corebusiness/users/GetLoggedUserServiceImpl.java
@@ -2,6 +2,8 @@ package br.com.jcv.treinadorpro.corebusiness.users;
 
 import br.com.jcv.commons.library.commodities.exception.CommoditieBaseException;
 import br.com.jcv.commons.library.commodities.response.ControllerGenericResponse;
+import br.com.jcv.restclient.dto.SessionStateDTO;
+import br.com.jcv.restclient.guardian.GuardianRestClientConsumer;
 import br.com.jcv.treinadorpro.corelayer.response.PersonalTrainerResponse;
 import br.com.jcv.treinadorpro.infrastructure.interceptor.RequestTokenHolder;
 import br.com.jcv.treinadorpro.infrastructure.utils.ControllerGenericResponseHelper;
@@ -12,18 +14,36 @@ import org.springframework.stereotype.Service;
 import java.util.UUID;
 
 @Service
-public class GetLoggedUserServiceImpl implements GetLoggedUserService{
+public class GetLoggedUserServiceImpl implements GetLoggedUserService {
+
+    private final GuardianRestClientConsumer guardianRestClientConsumer;
+    private final FindPersonalTrainerByGuardianIdService findPersonalTrainerByGuardianIdService;
+
+    public GetLoggedUserServiceImpl(GuardianRestClientConsumer guardianRestClientConsumer,
+                                    FindPersonalTrainerByGuardianIdService findPersonalTrainerByGuardianIdService) {
+        this.guardianRestClientConsumer = guardianRestClientConsumer;
+        this.findPersonalTrainerByGuardianIdService = findPersonalTrainerByGuardianIdService;
+    }
+
     @Override
     public ControllerGenericResponse<PersonalTrainerResponse> execute(UUID processId) {
-        try {
-            PersonalTrainerResponse personalTrainer = RequestTokenHolder.getPersonalTrainer();
-            return ControllerGenericResponseHelper.getInstance(
-                    "MSG-1328",
-                    "User logged has been retrieved successfully",
-                    personalTrainer
-            );
-        } catch (JsonProcessingException e) {
-            throw new CommoditieBaseException("MSG-1329", HttpStatus.UNPROCESSABLE_ENTITY, "It wasn't possible to recover logged user");
-        }
+        PersonalTrainerResponse personalTrainer = getTrainerFromToken(RequestTokenHolder.getToken());
+        return ControllerGenericResponseHelper.getInstance(
+                "MSG-1328",
+                "User logged has been retrieved successfully",
+                personalTrainer
+        );
+
     }
+
+    private PersonalTrainerResponse getTrainerFromToken(String token) {
+
+        ControllerGenericResponse<SessionStateDTO> sessionState = guardianRestClientConsumer.findSessionState(token);
+
+        ControllerGenericResponse<PersonalTrainerResponse> personalTrainer = findPersonalTrainerByGuardianIdService
+                .execute(UUID.randomUUID(), sessionState.getObjectResponse().getIdUserUUID());
+        return personalTrainer.getObjectResponse();
+    }
+
+
 }
